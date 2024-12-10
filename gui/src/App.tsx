33aa97f2caeaa8d93319f667f239cb9f23306d66
philippebner <29914922+philippebner@users.noreply.github.com>
2024-12-10 12:55:25 +0100

    Update color scheme
    Signed-off-by: philippebner <29914922+philippebner@users.noreply.github.com>

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -4,61 +4,45 @@ import { CssVarsProvider, extendTheme } from '@mui/joy'
 
 function App() {
 
+  const baseColor = '#016fb6'
+  const lf = 0.2
+
   const theme = extendTheme({
     colorSchemes: {
       dark: {
         palette: {
           primary: {
-            50: '#64b6f7',
-            100: '#4dabf5',
-            200: '#37a1f4',
-            300: '#2196F3',
-            400: '#1a78c2',
-            500: '#145a92',
-            600: '#0d3c61',
-            700: '#0a2d49',
-            800: '#071e31',
-            900: '#030f18',
+            50: `hsl(from ${baseColor} h s calc(l + l * ${5 * lf}))`,
+            100: `hsl(from ${baseColor} h s calc(l + l * ${4 * lf}))`,
+            200: `hsl(from ${baseColor} h s calc(l + l * ${3 * lf}))`,
+            300: `hsl(from ${baseColor} h s calc(l + l * ${2 * lf}))`,
+            400: `hsl(from ${baseColor} h s calc(l + l * ${lf}))`,
+            500: baseColor,
+            600: `hsl(from ${baseColor} h s calc(l - l * ${lf}))`,
+            700: `hsl(from ${baseColor} h s calc(l - l * ${2 * lf}))`,
+            800: `hsl(from ${baseColor} h s calc(l - l * ${3 * lf}))`,
+            900: `hsl(from ${baseColor} h s calc(l - l * ${4 * lf}))`,
           },
         },
       },
       light: {
         palette: {
           primary: {
-            50: '#bce0fb',
-            100: '#a6d5fa',
-            200: '#90cbf9',
-            300: '#64b6f7',
-            400: '#37a1f4',
-            500: '#2196F3',
-            600: '#1a78c2',
-            700: '#145a92',
-            800: '#0d3c61',
-            900: '#0a2d49',
+            50: `hsl(from ${baseColor} h s calc(l + l * ${5 * lf}))`,
+            100: `hsl(from ${baseColor} h s calc(l + l * ${4 * lf}))`,
+            200: `hsl(from ${baseColor} h s calc(l + l * ${3 * lf}))`,
+            300: `hsl(from ${baseColor} h s calc(l + l * ${2 * lf}))`,
+            400: `hsl(from ${baseColor} h s calc(l + l * ${lf}))`,
+            500: baseColor,
+            600: `hsl(from ${baseColor} h s calc(l - l * ${lf}))`,
+            700: `hsl(from ${baseColor} h s calc(l - l * ${2 * lf}))`,
+            800: `hsl(from ${baseColor} h s calc(l - l * ${3 * lf}))`,
+            900: `hsl(from ${baseColor} h s calc(l - l * ${4 * lf}))`,
           },
         },
       },
     },
   });
-
-  /**
-   * dark: {
-        palette: {
-          primary: {
-            50: '#b270e9',
-            100: '#a760e0',
-            200: '#9d4edd',
-            300: '#8d46c7',
-            400: '#7e3eb1',
-            500: '#6e379b',
-            600: '#5e2f85',
-            700: '#4f276f',
-            800: '#3f1f58',
-            900: '#2f1742',
-          },
-        },
-      },
-   */
 
   return (
     <>
